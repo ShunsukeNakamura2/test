d4bbefea8beec26b3ede9a783671bd5aa7dec3b3
中村 俊介 <H1018290@hulft.saison.co.jp>
2024-03-19 16:17:00 +0900

    add: kadai10

---
+++ kadai10/kadai10.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * Z‰Û‘è10
+ * intŒ^‚Ì2‚Â‚Ì•Ï”‚Ì’l‚ğ“ü‚ê‘Ö‚¦‚éŠÖ”‚ğ
+ * ì¬‚µ‚Ä‚­‚¾‚³‚¢B
+*/
+
+void swap_int(int *a, int *b);
+
+int main()
+{
+    int a = 1;
+    int b = 2;
+
+    printf("“ü‚ê‘Ö‚¦‘O\na=%d b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("“ü‚ê‘Ö‚¦Œã\na=%d b=%d\n", a, b);
+
+    return 0;
+}
+
+void swap_int(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
