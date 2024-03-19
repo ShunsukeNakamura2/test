e475861dc6d63eff405f925344bfb40431330193
中村 俊介 <H1018290@hulft.saison.co.jp>
2024-03-19 13:26:13 +0900

    add:kadai3

---
+++ kadai3/kadai3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * 〇課題3
+ * 任意の文字列をcharの配列に読み込み、その配列のサイズ(バイト数)
+ * と文字列長を表示してください。
+ * またその文字列の最初の1文字と最後の1文字を表示してください。
+*/
+int main()
+{
+    char str[] = "ShunsukeNakamura";
+    printf("配列サイズ=%d 文字列長=%d\n", sizeof(str), strlen(str));
+    printf("1文字目は%c、最後の文字は%cです。\n",str[0], str[strlen(str)-1]);
+    return 0;
+}
