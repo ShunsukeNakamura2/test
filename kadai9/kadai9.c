aaa1410a9708c3ff213963ee86ba39f70a5ee3e2
中村 俊介 <H1018290@hulft.saison.co.jp>
2024-03-21 16:10:29 +0900

    kadai9

---
+++ kadai9/kadai9.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * 〇課題9
+ * コマンドラインで渡されたパラメータを
+ * 全て表示するプログラムを作成してください。
+*/
+
+int main(int argc, char *argv[])
+{
+    for(int i = 1; i < argc; i++) {
+        printf("%d : %s\n", i, argv[i]);
+    }
+    return 0;
+}
