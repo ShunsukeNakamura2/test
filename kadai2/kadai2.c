4d10d2a547857cdfe19eefc19f198b5e7139193c
中村 俊介 <H1018290@hulft.saison.co.jp>
2024-03-22 07:53:47 +0900

    modify:kadai2.c

--- kadai2/kadai2.c
+++ kadai2/kadai2.c
@@ -9,9 +9,9 @@
 
 int main(int argc, char *argv[])
 {
-    printf("char=%05d short=%05d int=%05d long=%05d float=%05d double=%05d\n", 
-                                                sizeof(char), sizeof(short), 
-                                                sizeof(int), sizeof(long), 
+    printf("char=%05d short=%05d int=%05d long=%05d float=%05d double=%05d\n",
+                                                sizeof(char), sizeof(short),
+                                                sizeof(int), sizeof(long),
                                                 sizeof(float), sizeof(double));
     return 0;
 }
