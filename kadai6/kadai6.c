ddf633e902f8a229cb0abb5b1057a07a8780dfd9
中村 俊介 <H1018290@hulft.saison.co.jp>
2024-03-22 07:56:43 +0900

    modify: kadai6.c

--- kadai6/kadai6.c
+++ kadai6/kadai6.c
@@ -2,9 +2,9 @@
 #include<stddef.h>
 
 /*
-〇課題6
-構造体「SAMPLE_t」のサイズ、各メンバのサイズの合計、
-各メンバの先頭からのオフセットとサイズを表示してください。
+* 〇課題6
+* 構造体「SAMPLE_t」のサイズ、各メンバのサイズの合計、
+* 各メンバの先頭からのオフセットとサイズを表示してください。
 */
 
 struct SAMPLE_t
@@ -21,12 +21,12 @@ int main(int argc, char *argv[])
 {
     struct SAMPLE_t sample;
     /*
-    gccコンパイル時の文字化け対策で、造の前に\を入れている
-    https://www.novell.com/ja-jp/support/kb/tids/jp/00006500_/00006574.htm
+    * gccコンパイル時の文字化け対策で、造の前に\を入れている
+    * https://www.novell.com/ja-jp/support/kb/tids/jp/00006500_/00006574.htm
     */
-    printf("構\造体のサイズ=%d メンバの合計サイズ=%d\n", sizeof(sample), sizeof(sample.m1) + sizeof(sample.m2)
-                                                                     + sizeof(sample.m3) + sizeof(sample.m4)
-                                                                     + sizeof(sample.m5) + sizeof(sample.m6));
+    printf("構\造体のサイズ=%d メンバの合計サイズ=%d\n", sizeof(sample), sizeof(sample.m1) + sizeof(sample.m2) +
+                                                                     sizeof(sample.m3) + sizeof(sample.m4) +
+                                                                     sizeof(sample.m5) + sizeof(sample.m6));
     printf("char  m1オフセット=%d サイズ=%d\n", offsetof(struct SAMPLE_t, m1), sizeof(sample.m1));
     printf("short m2オフセット=%d サイズ=%d\n", offsetof(struct SAMPLE_t, m2), sizeof(sample.m2));
     printf("char  m3オフセット=%d サイズ=%d\n", offsetof(struct SAMPLE_t, m3), sizeof(sample.m3));
