8d227785e2cd68276b5a7b811d8e7d4602bf1863
ShunsukeNakamura2 <154311504+ShunsukeNakamura2@users.noreply.github.com>
2024-03-22 12:38:17 +0900

    kadai2 (#2)
    * kadai2
    
    * modify: kadai2.c
    
    * modify: kadai2.c
    
    * modify:kadai2.c
    
    ---------
    
    Co-authored-by: 中村 俊介 <H1018290@hulft.saison.co.jp>

---
+++ kadai2/kadai2.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/*
+* char,short,int,long,float,double型の
+* 変数のサイズ(バイト数)を表示してください。
+* またサイズは5桁表示とし、5桁に満たない
+* 場合は0で詰めてください。
+*/
+
+int main(int argc, char *argv[])
+{
+    printf("char=%05d short=%05d int=%05d long=%05d float=%05d double=%05d\n",
+                                                sizeof(char), sizeof(short),
+                                                sizeof(int), sizeof(long),
+                                                sizeof(float), sizeof(double));
+    return 0;
+}
