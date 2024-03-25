932723f0647aa40df7d5a73c0df7022192a49d32
中村 俊介 <H1018290@hulft.saison.co.jp>
2024-03-25 11:45:48 +0900

    kadai19

---
+++ kadai19/kadai19.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/*
+* 〇課題19
+* 指定したサイズのファイルを作成する
+* プログラムを作成してください。
+* (パラメータsizeの値が格納するデータ型の
+* 範囲外の値かチェックする必要はありません)
+*/
+
+#define MEGA 1048576
+#define KILO 1024
+
+int main(int argc, char *argv[])
+{
+    int param_length;
+    int size;
+    char *size_char;
+    char prefix;
+    FILE *fp;
+    int fopen_error;
+
+    param_length = strlen(argv[2]);
+    prefix = argv[2][param_length - 1];
+
+    size_char = malloc(param_length);
+    strncpy(size_char, argv[2], param_length - 1);
+    size = atoi(size_char);
+    free(size_char);
+
+    if(prefix == 'M') {
+        size *= MEGA;
+    } else if(prefix == 'K') {
+        size *= KILO;
+    }
+
+    fopen_error = fopen_s(&fp, argv[1], "wb");    
+    if(fopen_error != 0) {
+        printf("file open error\nerror code:%d", fopen_error);
+        return 0;
+    }
+
+    for(int i = 0; i < size; i++) {
+        fputc('\0', fp);
+    }
+    fclose(fp);
+
+    return 0;
+}
