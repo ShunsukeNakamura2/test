a038bc7cb967a4cef883d059b74691db1d129e9d
ShunsukeNakamura2 <154311504+ShunsukeNakamura2@users.noreply.github.com>
2024-03-25 12:08:14 +0900

    kadai8 (#8)
    * kadai8
    
    * modify: kadai8.c
    
    * modify: kadai8.c
    
    * modify: kadai8.c
    
    * modify: kadai8.c, 実行結果.txt
    
    * :change encoding
    
    * modify: kadai8.c
    
    ---------
    
    Co-authored-by: 中村 俊介 <H1018290@hulft.saison.co.jp>

---
+++ kadai8/kadai8.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+/*
+* 〇課題8
+* プログラムを起動してから呼び出すたびに
+* 1インクリメントした値を返す関数を作成してください。
+* 初期値は0とします。
+* (intの最大値を超えた場合の動作を考慮する必要はありません)
+*/
+
+static int static_counter();
+
+int main(int argc, char *argv[])
+{
+    for(int i = 0; i < 10; i++) {
+        int count = static_counter();
+        printf("%d\n", count);
+    }
+    return 0;
+}
+
+/**
+ * @brief 呼び出されるたびに1インクリメントされた値を返す
+ * 
+ * @return 呼び出された回数
+*/
+static int static_counter()
+{
+    static int count = 0;
+    count++;
+    return count;
+}
