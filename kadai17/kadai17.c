8990a9995b91577cb13ece2ff97f4930dbaa154f
俊介 中村 <Shunsuke_Nakamura@sis2.saison.co.jp>
2024-03-25 17:45:14 +0900

    kadai17

--- kadai17/kadai17.c
+++ kadai17/kadai17.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 /*
 * 〇課題17
@@ -13,29 +14,45 @@
 #define MEGA 1048576
 #define KILO 1024
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
     int param_length;
-    int size;
-    char *size_char;
-    char prefix;
+    int size = 0;
+    char last_char;
     void *memory;
-    
+    int rep_num;
+
+    if (argc < 2) {
+        printf("usage: kadai17 size[M|K]\noptions: size[M | K]  サイズ(M = メガ, K = キロ)");
+        return 0;
+    }
+
     param_length = strlen(argv[1]);
-    prefix = argv[1][param_length - 1];
+    rep_num = param_length - 1;
+    for(int i = 0; i < rep_num; i++) {
+        size *= 10;
+        char buff = argv[1][i];
+        if (!isdigit(buff)) {
+            printf("usage: kadai17 size[M|K]\noptions: size[M | K]  サイズ(M = メガ, K = キロ)");
+            return 0;
+        }
+        size += buff - '0';
+    }
 
-    size_char = malloc(param_length);
-    strncpy(size_char, argv[1], param_length - 1);
-    size = atoi(size_char);
-    free(size_char);
-
-    if(prefix == 'M') {
+    last_char = argv[1][param_length - 1];
+    if(isdigit(last_char)) {
+        size = size * 10 + (last_char - '0');
+    } else if (last_char == 'M') {
         size *= MEGA;
-    } else if(prefix == 'K') {
+    } else if (last_char == 'K') {
         size *= KILO;
+    } else {
+        printf("usage: kadai17 size[M|K]\noptions: size[M | K]  サイズ(M = メガ, K = キロ)");
+        return 0;
     }
 
     memory = malloc(size);
+    free(memory);
 
     return 0;
 }
