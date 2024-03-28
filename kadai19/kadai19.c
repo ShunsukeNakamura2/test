aee559429c4f736014801e66c80b190054e8f0fb
俊介 中村 <Shunsuke_Nakamura@sis2.saison.co.jp>
2024-03-29 08:41:37 +0900

    modify: kadai19.c 実行結果.txt Makefile

--- kadai19/kadai19.c
+++ kadai19/kadai19.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-
+#include <fcntl.h>
+#include <unistd.h>
 /*
 * 〇課題19
 * 指定したサイズのファイルを作成する
@@ -14,8 +14,13 @@
 #define MEGA 1048576
 #define KILO 1024
 #define MSG_USAGE "usage: kadai19 filename size[M|K]\noptions: filename  ファイル名size[M|K]\n             サイズ(M=メガ,K=キロ)\n"
+#define MSG_FILE_ERROR "file %s error\nerror:%s (code:%d)\n"
+#define RETURN_NOMAL_END 0
 #define RETURN_USAGE 1
-#define RETURN_FAIL_FILE_OPEN 2
+#define RETURN_FAIL_OPEN 2
+#define RETURN_FAIL_CLOSE 3
+#define RETURN_FAIL_WRITE 4
+#define RETURN_FAIL_WRITE_CLOSE 5
 
 int main(int argc, char *argv[])
 {
@@ -23,15 +28,19 @@ int main(int argc, char *argv[])
     int size = 0;
     char last_char;
     int rep_num;
-    char *request_file_name = argv[1];
-    char *request_size_str = argv[2];
-    FILE *fp;
+    char *request_file_name;
+    char *request_size_str;
+    int fd;
     int rc;
+    int main_rc = RETURN_NOMAL_END;
+    
 
     if (argc < 3) {
         printf(MSG_USAGE);
         return RETURN_USAGE;
     }
+    request_file_name = argv[1];
+    request_size_str = argv[2];
 
     param_length = strlen(request_size_str);
     /* 末尾を除く文字を処理 */
@@ -59,22 +68,23 @@ int main(int argc, char *argv[])
         return RETURN_USAGE;
     }
 
-    rc = fopen_s(&fp, request_file_name, "wb");
-    if(rc != 0) {
-        char *error_message = strerror(rc);
-        printf("file open error\nerror:%s (code:%d)\n", error_message, rc);
-        return RETURN_FAIL_FILE_OPEN;
+    fd = open(request_file_name, O_RDWR | O_CREAT | O_TRUNC | O_BINARY, 0644);
+    if(fd == -1) {
+        printf(MSG_FILE_ERROR, "open", strerror(errno), errno);
+        return RETURN_FAIL_OPEN;
     }
 
-    for(int i = 0; i < size; i++) {
-        fputc('\0', fp);
-    }
+    lseek(fd, size - 1, SEEK_SET);
+    write(fd, "\0", 1);
 
-    rc = fclose(fp);
-     if(rc != 0) {
-        char *error_message = strerror(rc);
-        printf("file close error\nerror:%s (code:%d)\n", error_message, rc);
-        return RETURN_FAIL_FILE_OPEN;
+    rc = close(fd);
+    if(rc == -1) {
+        printf(MSG_FILE_ERROR, "close", strerror(errno), errno);
+        if(main_rc == RETURN_FAIL_WRITE) {
+            main_rc = RETURN_FAIL_WRITE_CLOSE;
+        } else {
+            main_rc = RETURN_FAIL_CLOSE;
+        }
     }
-    return 0;
+    return main_rc;
 }
