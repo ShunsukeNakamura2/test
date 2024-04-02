29e218b44f92c8ad341e5e328bb2d12ad67c15b8
俊介 中村 <shunsuke_nakamura@saison-technology.com>
2024-04-02 15:07:48 +0900

    modify:kadai20.c

--- kadai20/kadai20.c
+++ kadai20/kadai20.c
@@ -19,106 +19,138 @@
 #define RETURN_FAIL_OPEN 2
 #define RETURN_FAIL_CLOSE 3
 #define RETURN_FAIL_WRITE 4
-#define RETURN_FAIL_WRITE_CLOSE 5
-#define TYPE_B2 0
-#define TYPE_B4 1
-#define TYPE_F4 2
+#define ARGC 7
+
+typedef struct st_args {
+    void *number;
+    char *type;
+    char *filename;
+    int size_of_type;
+} st_args;
+
+static int analyze_args(int argc, char *argv[], st_args *args_val);
 
 int main(int argc, char *argv[])
 {
     int main_rc = RETURN_NORMAL_END;
     int rc;
-    int pos_n = -1; /* パラメータ-nの引数上の位置 */
-    int pos_t = -1; /* パラメータ-tの引数上の位置 */
-    int pos_f = -1; /* パラメータ-fの引数上の位置 */
-    short param_number_B2;
-    int   param_number_B4;
-    float param_number_F4;
-    char *param_type_str;
-    int   param_type;
-    char *param_filename;
-    int   rep_num;
-    char *endptr;
-    int   fd;
+    int rep_num;
+    int fd;
 
-    if(argc != 7) {
+    st_args params = {NULL, NULL, NULL, 0};
+    rc = analyze_args(argc, argv, &params);
+    if(rc == -1) {
         printf(MSG_USAGE);
         return RETURN_USAGE;
     }
 
-    /* オプションの位置を取得 */
-    rep_num = argc - 1;
-    for(int i = 1; i < rep_num; i++) {
-        if(strcmp(argv[i], "-n") == 0) {
-            pos_n = i;
-        } else if(strcmp(argv[i], "-t") == 0) {
-            pos_t = i;
-        } else if(strcmp(argv[i], "-f") == 0) {
-            pos_f = i;
-        }
-    }
-
-    if(pos_n == -1 || pos_t == -1 || pos_f == -1) {
-        printf(MSG_USAGE);
-        return RETURN_USAGE;
-    }
-
-    /* パラメータtype, numberを取得 */
-    param_type_str = argv[pos_t + 1];
-    endptr = NULL;
-    if(strcmp(param_type_str, "B2") == 0) {
-        param_type = TYPE_B2;
-        param_number_B2 = (short)strtol(argv[pos_n + 1], &endptr, 10);
-    } else if(strcmp(param_type_str, "B4") == 0) {
-        param_type = TYPE_B4;
-        param_number_B4 = (int)strtol(argv[pos_n + 1], &endptr, 10);
-    } else if(strcmp(param_type_str, "F4") == 0) {
-        param_type = TYPE_F4;
-        param_number_F4 = (float)strtod(argv[pos_n + 1], &endptr);
-    } else {
-        printf(MSG_USAGE);
-        return RETURN_USAGE;
-    }
-    if (*endptr != '\0') {
-        printf(MSG_USAGE);
-        return RETURN_USAGE;
-    }
-
-    param_filename = argv[pos_f + 1];
-    fd = open(param_filename, O_WRONLY | O_CREAT | O_APPEND | O_BINARY,
+    fd = open(params.filename, O_WRONLY | O_CREAT | O_APPEND | O_BINARY,
                             S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
     if(fd == -1) {
         printf(MSG_FILE_ERROR, "open", strerror(errno), errno);
         return RETURN_FAIL_OPEN;
     }
 
-    switch (param_type) {
-    case TYPE_B2:
-        rc = write(fd, &param_number_B2, sizeof(param_number_B2));
-        break;
-    case TYPE_B4:
-        rc = write(fd, &param_number_B4, sizeof(param_number_B4));
-        break;
-    case TYPE_F4:
-        rc = write(fd, &param_number_F4, sizeof(param_number_F4));
-        break;
-    default:
-        break;
-    }
+    rc = write(fd, params.number, params.size_of_type);
     if(rc == -1) {
         printf(MSG_FILE_ERROR, "write", strerror(errno), errno);
-        main_rc = RETURN_FAIL_OPEN;
+        main_rc = RETURN_FAIL_WRITE;
+        goto end;
     }
 
+end:
     rc = close(fd);
     if(rc == -1) {
         printf(MSG_FILE_ERROR, "close", strerror(errno), errno);
-        if(main_rc == RETURN_FAIL_OPEN) {
-            main_rc = RETURN_FAIL_WRITE_CLOSE;
-        } else {
+        if(main_rc == RETURN_NORMAL_END) {
             main_rc = RETURN_FAIL_CLOSE;
         }
     }
     
     return main_rc;
+}
+
+/**
+ * @brief コマンドライン引数をチェックしパラメータを構造体st_argsに入れる
+ * 
+ * @param[in] argc コマンドライン引数の個数
+ * @param[in] argv コマンドライン引数
+ * @param[in,out] args_val パラメータ格納用構造体
+ * 
+ * @retval 0   正常終了
+ * @retval -1  コマンドライン引数不正
+*/
+static int analyze_args(int argc, char *argv[], st_args *args_val)
+{
+    char *param_number_str = NULL;
+    short param_number_B2;
+    int   param_number_B4;
+    float param_number_F4;
+    char *endptr = NULL;
+
+    if(argc != ARGC) {
+        return -1;
+    }
+
+    args_val->type = NULL;
+    args_val->filename = NULL;
+
+    /* オプションとパラメータを設定 */
+    for(int i = 1; i < argc; i++) {
+        if(strcmp(argv[i], "-n") == 0) {
+            /* numberを取得 */
+            if(param_number_str != NULL) {
+                return -1;
+            }
+            if(++i == argc) {
+                return -1;
+            }
+            param_number_str = argv[i];
+        } else if(strcmp(argv[i], "-t") == 0) {
+            /* typeを取得 */
+            if(args_val->type != NULL) {
+                return -1;
+            }
+            if(++i == argc) {
+                return -1;
+            }
+            args_val->type = argv[i];
+        } else if(strcmp(argv[i], "-f") == 0) {
+            /* filenameを取得 */
+            if(args_val->filename != NULL) {
+                return -1;
+            }
+            if(++i == argc) {
+                return -1;
+            }
+            args_val->filename = argv[i];
+        }
+    }
+
+    /* オプションに不足がある場合失敗 */
+    if(param_number_str == NULL || args_val->type == NULL || args_val->filename == NULL) {
+        return -1;
+    }
+
+    /* 対応したtypeの数値を格納 */
+    if(strcmp(args_val->type, "B2") == 0) {
+        param_number_B2 = (short)strtol(param_number_str, &endptr, 10);
+        args_val->number = &param_number_B2;
+        args_val->size_of_type = sizeof(param_number_B2);
+    } else if(strcmp(args_val->type, "B4") == 0) {
+        param_number_B4 = (int)strtol(param_number_str, &endptr, 10);
+        args_val->number = &param_number_B4;
+        args_val->size_of_type = sizeof(param_number_B4);
+    } else if(strcmp(args_val->type, "F4") == 0) {
+        param_number_F4 = (float)strtod(param_number_str, &endptr);
+        args_val->number = &param_number_F4;
+        args_val->size_of_type = sizeof(param_number_F4);
+    } else {
+        return -1;
+    }
+    /* 文字列 -> 数値変換失敗 */
+    if (*endptr != '\0') {
+        return -1;
+    }
+    return 0;
 }
