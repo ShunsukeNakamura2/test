6e73ddb4536674e047ed5a3d8d73ed2c94f9d230
ShunsukeNakamura2 <154311504+ShunsukeNakamura2@users.noreply.github.com>
2024-04-02 10:36:10 +0900

    kadai19 (#19)
    * kadai19
    
    * modify:kadai19.c
    
    * modify: kadai19.c 実行結果.txt Makefile
    
    * modify: kadai19.c
    
    * modify:kadai19.c
    
    * modify:kadai19.c
    
    * modify:kadai19.c
    
    * modify:kadai19.c
    
    ---------
    
    Co-authored-by: 中村 俊介 <H1018290@hulft.saison.co.jp>

---
+++ kadai19/kadai19.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+/*
+* 〇課題19
+* 指定したサイズのファイルを作成する
+* プログラムを作成してください。
+* (パラメータsizeの値が格納するデータ型の
+* 範囲外の値かチェックする必要はありません)
+*/
+
+#define MEGA 1048576
+#define KILO 1024
+#define MSG_USAGE "usage: kadai19 filename size[M|K]\noptions: filename  ファイル名size[M|K]\n             サイズ(M=メガ,K=キロ)\n"
+#define MSG_FILE_ERROR "file %s error\nerror:%s (code:%d)\n"
+#define RETURN_NORMAL_END 0
+#define RETURN_USAGE 1
+#define RETURN_FAIL_OPEN 2
+#define RETURN_FAIL_CLOSE 3
+#define RETURN_FAIL_SEEK 4
+#define RETURN_FAIL_WRITE 5
+
+int main(int argc, char *argv[])
+{
+    int param_length;
+    int size = 0;
+    char last_char;
+    int rep_num;
+    char *request_file_name;
+    char *request_size_str;
+    int fd;
+    int rc;
+    int main_rc = RETURN_NORMAL_END;
+    
+
+    if (argc < 3) {
+        printf(MSG_USAGE);
+        return RETURN_USAGE;
+    }
+    request_file_name = argv[1];
+    request_size_str = argv[2];
+
+    param_length = strlen(request_size_str);
+    /* 末尾を除く文字を処理 */
+    rep_num = param_length - 1;
+    for(int i = 0; i < rep_num; i++) {
+        size *= 10;
+        char buff = request_size_str[i];
+        if (!isdigit(buff)) {
+            printf(MSG_USAGE);
+            return RETURN_USAGE;
+        }
+        size += buff - '0';
+    }
+
+    /* 末尾の文字を処理 */
+    last_char = request_size_str[param_length - 1];
+    if(isdigit(last_char)) {
+        size = size * 10 + (last_char - '0');
+    } else if (last_char == 'M') {
+        size *= MEGA;
+    } else if (last_char == 'K') {
+        size *= KILO;
+    } else {
+        printf(MSG_USAGE);
+        return RETURN_USAGE;
+    }
+
+    fd = open(request_file_name, O_WRONLY | O_CREAT | O_TRUNC | O_BINARY,
+                                S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if(fd == -1) {
+        printf(MSG_FILE_ERROR, "open", strerror(errno), errno);
+        return RETURN_FAIL_OPEN;
+    }
+
+    rc = lseek(fd, size - 1, SEEK_SET);
+    if(rc == -1) {
+        printf(MSG_FILE_ERROR, "seek", strerror(errno), errno);
+        main_rc = RETURN_FAIL_SEEK;
+        goto end;
+    } 
+    rc = write(fd, "\0", 1);
+    if(rc == -1) {
+        printf(MSG_FILE_ERROR, "write", strerror(errno), errno);
+        main_rc = RETURN_FAIL_WRITE;
+    }
+    
+end:
+    rc = close(fd);
+    if(rc == -1) {
+        printf(MSG_FILE_ERROR, "close", strerror(errno), errno);
+        if(main_rc == RETURN_NORMAL_END) {
+            main_rc = RETURN_FAIL_CLOSE;
+        } 
+    }
+    return main_rc;
+}
