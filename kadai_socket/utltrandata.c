2f43df3a0f31383a0ae270f9f0a0d7eeca425e5c
ShunsukeNakamura2 <shunsuke_nakamura@saison-technology.com>
2024-04-09 12:35:21 +0900

    modify: utltrandata.c

--- kadai_socket/utltrandata.c
+++ kadai_socket/utltrandata.c
@@ -18,7 +18,7 @@
 * 受信側は、送られてきた情報を、指定したファイルに追記する
 * */
 
-#define MSG_USAGE \
+#define MSG_ERR_USAGE \
 	"usage:\n"\
 	"utltrandata -s portno -h hostname -f filename\n"\
 	"	-h hostname	指定されたホストに対して接続します。\n"\
@@ -27,33 +27,33 @@
 	"utltrandata -r portno -f filename\n"\
 	"	-r portno	指定されたポート番号で接続を待ちます。\n"\
 	"	-f filename	指定されたファイルにデータを追記します。\n"
-#define MSG_ERROR_COMMON "error:%s (code:%d)\n"
-#define MSG_FILE_ERROR "file %s error\n"MSG_ERROR_COMMON
-#define MSG_CLOSE_ERROR "failed to close %s\n"MSG_ERROR_COMMON
-#define MSG_FORK_ERROR "fork error\n"MSG_ERROR_COMMON
-#define MSG_GET_OPEN_MAX_ERROR "failed to get file descriptor openmax\n"MSG_ERROR_COMMON
-#define MSG_SOCKET_ERROR "failed to socket\n"MSG_ERROR_COMMON
-#define MSG_BIND_ERROR "failed to bind\n"MSG_ERROR_COMMON
-#define MSG_LISTEN_ERROR "failed to listen\n"MSG_ERROR_COMMON
-#define MSG_ACCEPT_ERROR "failed to accept\n"MSG_ERROR_COMMON
-#define MSG_SEND_ERROR "failed to send\n"MSG_ERROR_COMMON
-#define MSG_RECV_ERROR "failed to recv\n"MSG_ERROR_COMMON
-#define MSG_NAME_RESOL_ERROR "failed to host name resolution\n"MSG_ERROR_COMMON
-#define MSG_CONNECT_ERROR "failed to connect\n"MSG_ERROR_COMMON
-#define MSG_SELECT_ERROR "failed to select\n"MSG_ERROR_COMMON
-#define MSG_SEND_RETRY "failed to send > retry...\n"
-#define MSG_RECV_RETRY "failed to receive > retry...\n"
-#define MSG_SEND_TIMEOUT "send:socket timeout\n"
-#define MSG_RECV_TIMEOUT "recv:socket timeout\n"
-#define MSG_DAEMON_INIT_ERROR "failed to daemonize process\n"
-#define MSG_SEND_PROCESS_ERROR "failed to send process\n"
-#define MSG_RECV_PROCESS_ERROR "failed to receive process\n"
-#define MSG_USE_WELL_KNOUN_PORT "cannot use well known port\n"
+#define MSG_ERR_COMMON "error:%s (code:%d)\n"
+#define MSG_ERR_FILE "file %s error\n"MSG_ERR_COMMON
+#define MSG_ERR_CLOSE "failed to close %s\n"MSG_ERR_COMMON
+#define MSG_ERR_FORK "fork error\n"MSG_ERR_COMMON
+#define MSG_ERR_GET_OPEN_MAX "failed to get file descriptor openmax\n"MSG_ERR_COMMON
+#define MSG_ERR_SOCKET "failed to socket\n"MSG_ERR_COMMON
+#define MSG_ERR_BIND "failed to bind\n"MSG_ERR_COMMON
+#define MSG_ERR_LISTEN "failed to listen\n"MSG_ERR_COMMON
+#define MSG_ERR_ACCEPT "failed to accept\n"MSG_ERR_COMMON
+#define MSG_ERR_SEND "failed to send\n"MSG_ERR_COMMON
+#define MSG_ERR_RECV "failed to recv\n"MSG_ERR_COMMON
+#define MSG_ERR_NAME_RESOL "failed to host name resolution\n"MSG_ERR_COMMON
+#define MSG_ERR_CONNECT "failed to connect\n"MSG_ERR_COMMON
+#define MSG_ERR_SELECT "failed to select\n"MSG_ERR_COMMON
+#define MSG_ERR_DAEMON_INIT "failed to daemonize process\n"
+#define MSG_ERR_SEND_PROCESS "failed to send process\n"
+#define MSG_ERR_RECV_PROCESS "failed to receive process\n"
+#define MSG_ERR_SEND_RETRY "failed to send > retry...\n"
+#define MSG_ERR_RECV_RETRY "failed to receive > retry...\n"
+#define MSG_ERR_SEND_TIMEOUT "send:socket timeout\n"
+#define MSG_ERR_RECV_TIMEOUT "recv:socket timeout\n"
+#define MSG_ERR_USE_WELL_KNOUN_PORT "cannot use well known port\n"
 #define RETURN_NORMAL_END 0
-#define RETURN_USAGE 1
-#define RETURN_FAIL_DAEMONIZE 2
-#define RETURN_FAIL_SEND 3
-#define RETURN_FAIL_RECV 4
+#define RETURN_ERR_USAGE 1
+#define RETURN_ERR_DAEMONIZE 2
+#define RETURN_ERR_SEND 3
+#define RETURN_ERR_RECV 4
 #define MODE_SEND 1
 #define MODE_RECV 2
 #define ARGC_MIN 5
@@ -75,46 +75,44 @@ static int get_open_max();
 static int daemon_init();
 static int send_process(st_args params);
 static int recv_process(st_args params);
-static int recv_listen(unsigned short portno);
+static int prepare_recv_socket(unsigned short portno);
 static int recv_data(int client_fd, char *filename);
 static int send_connect(char *hostname, unsigned short portno);
 
 int main(int argc, char *argv[]) {
-	int main_rc = RETURN_NORMAL_END;
 	int rc;
 	st_args params = {NULL, NULL, 0, -1};
 
 	rc = analyze_args(argc, argv, &params);
 	if(rc == -1) {
-		printf(MSG_USAGE);
-		return RETURN_USAGE;
+		fprintf(stderr, MSG_ERR_USAGE);
+		return RETURN_ERR_USAGE;
 	}
 
 	switch(params.mode) {
 	case MODE_SEND:
 		rc = send_process(params);
 		if(rc == -1) {
-			fprintf(stderr, MSG_SEND_PROCESS_ERROR);
-			return RETURN_FAIL_SEND;
+			fprintf(stderr, MSG_ERR_SEND_PROCESS);
+			return RETURN_ERR_SEND;
 		}
 		break;
 	case MODE_RECV:
 		rc = daemon_init();
 		if(rc == -1) {
-			fprintf(stderr, MSG_DAEMON_INIT_ERROR);
-			return RETURN_FAIL_DAEMONIZE;
+			fprintf(stderr, MSG_ERR_DAEMON_INIT);
+			return RETURN_ERR_DAEMONIZE;
 		}
 		rc = recv_process(params);
 		if(rc == -1) {
-			fprintf(stderr, MSG_RECV_PROCESS_ERROR);
-			return RETURN_FAIL_RECV;
+			fprintf(stderr, MSG_ERR_RECV_PROCESS);
+			return RETURN_ERR_RECV;
 		}
 		break;
 	default:
-		
 		break;
 	}
-	return main_rc;
+	return RETURN_NORMAL_END;
 }
 
 /**
@@ -132,15 +130,13 @@ static int analyze_args(int argc, char *argv[], st_args *params)
 	char *portno_str = NULL;
 	char *endptr = NULL;
 	int i;
-	int buf;
+	int tmp_portno;
 
 	if(argc < ARGC_MIN ) {
 		return -1;
 	}
-
-	params->hostname = NULL;
-	params->filename = NULL;
-	params->mode = -1;
+	
+	memset(params, 0, sizeof(st_args));
 
 	/* オプションとパラメータを取得 */
 	for(i = 0; i < argc; i++) {
@@ -149,7 +145,7 @@ static int analyze_args(int argc, char *argv[], st_args *params)
 			if(params->hostname != NULL) {
 				return -1;
 			}
-			if(++i == argc) {
+			if(++i >= argc) {
 				return -1;
 			}
 			params->hostname = argv[i];
@@ -158,26 +154,26 @@ static int analyze_args(int argc, char *argv[], st_args *params)
 			if(params->filename != NULL) {
 				return -1;
 			}
-			if(++i == argc) {
+			if(++i >= argc) {
 				return -1;
 			}
 			params->filename = argv[i];
 		} else if(strcmp(argv[i], "-s") == 0) {
 			/* 送信モード＆ポート番号 */
-			if(params->mode != -1) {
+			if(params->mode != 0) {
 				return -1;
 			}
-			if(++i == argc) {
+			if(++i >= argc) {
 				return -1;
 			}
 			params->mode = MODE_SEND;
 			portno_str = argv[i];
 		} else if(strcmp(argv[i], "-r") == 0) {
 			/* 受信モード＆ポート番号 */
-			if(params->mode != -1) {
+			if(params->mode != 0) {
 				return -1;
 			}
-			if(++i == argc) {
+			if(++i >= argc) {
 				return -1;
 			}
 			params->mode = MODE_RECV;
@@ -186,29 +182,32 @@ static int analyze_args(int argc, char *argv[], st_args *params)
 	}
 
 	/* パラメータに過不足がある場合引数不正 */
-	if(params->mode == MODE_SEND) {
+	switch(params->mode) {
+	case(MODE_SEND):
 		if(params->hostname == NULL || params->filename == NULL || portno_str == NULL) {
 			return -1;
 		}
-	} else if(params->mode == MODE_RECV) {
+		break;
+	case(MODE_RECV):
 		/* 受信モードではhostnameを指定してはいけない */
 		if(params->hostname != NULL || params->filename == NULL || portno_str == NULL) {
 			return -1;
 		}
-	} else {
+		break;
+	default:
 		return -1;
 	}
 
-	buf = (int)strtol(portno_str, &endptr, 10);
+	tmp_portno = (int)strtol(portno_str, &endptr, 10);
 	/* 文字列 -> 数値 変換失敗 */
 	if(*endptr != '\0') {
 		return -1;
 	}
 	/* 範囲外のport番号 */
-	if(buf < 0 || buf > 65535) {
+	if(tmp_portno < 0 || tmp_portno > 65535) {
 		return -1;
 	}
-	params->portno = (unsigned short)buf;
+	params->portno = (unsigned short)tmp_portno;
 
 	return 0;
 }
@@ -227,7 +226,7 @@ static int daemon_init()
 
 	pid = fork();
 	if(pid < 0) {
-		fprintf(stderr, MSG_FORK_ERROR, strerror(errno), errno);
+		fprintf(stderr, MSG_ERR_FORK, strerror(errno), errno);
 		return -1;
 	} else if(pid != 0) {
 		exit(0);
@@ -240,7 +239,7 @@ static int daemon_init()
 	/* stderr以外のfdをclose */
 	rep_num = get_open_max();
 	if(rep_num == -1) {
-		fprintf(stderr, MSG_GET_OPEN_MAX_ERROR, strerror(errno), errno);
+		fprintf(stderr, MSG_ERR_GET_OPEN_MAX, strerror(errno), errno);
 		return -1;
 	}
 	close(0); /* stdin */
@@ -292,18 +291,17 @@ static int recv_process(st_args params)
 	int sock_fd = -1;
 	int client_fd = -1;
 	int rc;
-	int is_fail;
 	struct sockaddr_in client_addr;
 	socklen_t client_addr_len = sizeof(client_addr);
 
 	/* Well Known Ports が指定されていないかチェック */
 	if(0 <= params.portno && params.portno <= 1023) {
-		fprintf(stderr, MSG_USE_WELL_KNOUN_PORT);
+		fprintf(stderr, MSG_ERR_USE_WELL_KNOUN_PORT);
 		return -1;
 	}
 
 	/* 受信用のソケットを準備 */
-	sock_fd = recv_listen(params.portno);
+	sock_fd = prepare_recv_socket(params.portno);
 	if(sock_fd == -1) {
 		return -1;
 	}
@@ -312,67 +310,61 @@ static int recv_process(st_args params)
 		/* 接続を待つ */
 		client_fd = accept(sock_fd, (struct sockaddr *)&client_addr, &client_addr_len);
 		if(client_fd == -1) {
-			fprintf(stderr, MSG_ACCEPT_ERROR, strerror(errno), errno);
-			is_fail = 1;
+			fprintf(stderr, MSG_ERR_ACCEPT, strerror(errno), errno);
 			goto end;
 		}
 
 		/* データを受信してファイルに書き込む */
 		rc = recv_data(client_fd, params.filename);
 		if(rc == -1) {
-			is_fail = 1;
 			goto end;
 		}
 
 		/* ソケットを閉じて次の接続へ */
 		rc = close(client_fd);
+		client_fd = -1;
 		if(rc == -1) {
-			fprintf(stderr, MSG_CLOSE_ERROR, "client socket", strerror(errno), errno);
-			is_fail = 1;
+			fprintf(stderr, MSG_ERR_CLOSE, "client socket", strerror(errno), errno);
 			goto end;
 		}
 	}
 
 end:
 	/* fdを開いていたら閉じる */
-	if(sock_fd > 0) {
-		rc = close(sock_fd);
-		if(rc == -1) {
-			fprintf(stderr, MSG_CLOSE_ERROR, "server socket", strerror(errno), errno);
-		}	
-	}
 	if(client_fd > 0) {
 		rc = close(client_fd);
 		if(rc == -1) {
-			fprintf(stderr, MSG_CLOSE_ERROR, "client socket", strerror(errno), errno);
+			fprintf(stderr, MSG_ERR_CLOSE, "client socket", strerror(errno), errno);
 		}
 	}
-
-	if(is_fail) {
-		return -1;
+	if(sock_fd > 0) {
+		rc = close(sock_fd);
+		if(rc == -1) {
+			fprintf(stderr, MSG_ERR_CLOSE, "server socket", strerror(errno), errno);
+		}	
 	}
-	return 0;
+
+	return -1;
 }
 
 /**
- * @bfief 受信側のソケットを準備し、ファイルディスクリプタを返す
+ * @brief 受信側のソケットを準備し、ファイルディスクリプタを返す
  *
  * @param[in] portno    使用するポート番号
  * 
  * @retval > 0 作成したソケット
  * @retval -1  ソケット作成失敗
  */
-static int recv_listen(unsigned short portno)
+static int prepare_recv_socket(unsigned short portno)
 {
 	int sock_fd;
 	int rc;
-	int is_fail = 0;
 	struct sockaddr_in recv_addr;
 
 	/* ソケット作成(IP V4, TCP, プロトコルは自動選択) */
 	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sock_fd == -1) {
-		fprintf(stderr, MSG_SOCKET_ERROR, strerror(errno), errno);
+		fprintf(stderr, MSG_ERR_SOCKET, strerror(errno), errno);
 		return -1;
 	}
 
@@ -385,30 +377,26 @@ static int recv_listen(unsigned short portno)
 	/* バインド */
 	rc = bind(sock_fd, (struct sockaddr *)&recv_addr, sizeof(recv_addr));
 	if(rc == -1) {
-		fprintf(stderr, MSG_BIND_ERROR, strerror(errno), errno);
-		is_fail = 1;
+		fprintf(stderr, MSG_ERR_BIND, strerror(errno), errno);
 		goto end;
 	}
 
 	/* ソケットを接続待ちに設定 接続要求を最大5件まで待たせる */
 	rc = listen(sock_fd, 5);
-	if(rc == -1) {
-		fprintf(stderr, MSG_LISTEN_ERROR, strerror(errno), errno);
-		is_fail = 1;
-		goto end;
+	if(rc == 0) {
+		return sock_fd;
 	}
+	fprintf(stderr, MSG_ERR_LISTEN, strerror(errno), errno);
+	goto end;
 
 end:
 	/* 失敗時の後始末 */
-	if(is_fail) {
-		rc = close(sock_fd);
-		if(rc == -1) {
-			fprintf(stderr, MSG_CLOSE_ERROR, "server socket", strerror(errno), errno);
-		}
-		return -1;
+	rc = close(sock_fd);
+	if(rc == -1) {
+		fprintf(stderr, MSG_ERR_CLOSE, "server socket", strerror(errno), errno);
 	}
+	return -1;
 
-	return sock_fd;
 }
 
 /**
@@ -422,75 +410,77 @@ end:
  */
 static int recv_data(int client_fd, char *filename)
 {
-	int outfile_fd = -1;
+	int outfile_fd;
 	char buff[BUFF_SIZE];
-	int main_rc = 0;
 	int rc;
 	int retry_count;
-	ssize_t size;
+	ssize_t size = 0;
 	fd_set rfds; /* 読み込み可能か監視するfdの集合 */
 	struct timeval timeout;
 
 	outfile_fd = open(filename, O_WRONLY | O_CREAT | O_APPEND , S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
 	if(outfile_fd == -1) {
-		fprintf(stderr, MSG_FILE_ERROR, "open", strerror(errno), errno);
-		main_rc = -1;
-		goto end;
+		fprintf(stderr, MSG_ERR_FILE, "open", strerror(errno), errno);
+		return -1;
 	}
 
-	/* ソケットが読み込み可能か確認 */
 	FD_ZERO(&rfds);
 	FD_SET(client_fd, &rfds);
 	timeout.tv_sec = TIMEOUT_SEC;
 	timeout.tv_usec = 0;
-	rc = select(client_fd + 1, &rfds, NULL, NULL, &timeout);
-	if(rc < 0) {
-		fprintf(stderr, MSG_SELECT_ERROR, strerror(errno), errno);
-		main_rc = -1;
-		goto end;
-	}
-	if(rc == 0) {
-		fprintf(stderr, MSG_RECV_TIMEOUT);
-		main_rc = -1;
-		goto end;
-	}
 
 	while(1) {
 		memset(buff, 0, BUFF_SIZE);
 		/* recvを最大MAX_RECV_RETRY回リトライする */
 		for(retry_count = 0; retry_count < MAX_RECV_RETRY; retry_count++) {
+			/* ソケットが読み込み可能か確認 */
+			rc = select(client_fd + 1, &rfds, NULL, NULL, &timeout);
+			if(rc < 0) {
+				fprintf(stderr, MSG_ERR_SELECT, strerror(errno), errno);
+				goto end;
+			}
+			if(rc == 0) {
+				fprintf(stderr, MSG_ERR_RECV_TIMEOUT);
+				continue;
+			}
+
 			size = recv(client_fd, buff, BUFF_SIZE, 0);
 			if(size >= 0) {
 				break;
 			}
-			fprintf(stderr, MSG_RECV_RETRY);
+			if(errno == EINTR || errno == EAGAIN) {  /* retryしてもいいエラー */
+				fprintf(stderr, MSG_ERR_RECV_RETRY);
+				continue;
+			} else {  /* retryしても復旧できないエラー */
+				fprintf(stderr, MSG_ERR_RECV, strerror(errno), errno);
+				goto end;
+			}
 		}
-		if(size == -1) {
-			fprintf(stderr, MSG_RECV_ERROR, strerror(errno), errno);
-			main_rc = -1;
-			goto end;
-		}
+
 		if(size == 0) { /* 相手が接続を終わったと判断 */
 			break;
 		}
 
 		size = write(outfile_fd, buff, size);
 		if(size == -1) {
-			fprintf(stderr, MSG_FILE_ERROR, "write", strerror(errno), errno);
-			main_rc = -1;
+			fprintf(stderr, MSG_ERR_FILE, "write", strerror(errno), errno);
 			goto end;
 		}
 	}
 
-end:
-	if(outfile_fd > 0) {
-		rc= close(outfile_fd);
-		if(rc == -1) {
-			fprintf(stderr, MSG_FILE_ERROR, "close", strerror(errno), errno);
-			main_rc = -1;
-		}
+	rc = close(outfile_fd);
+	if(rc == -1) {
+		fprintf(stderr, MSG_ERR_FILE, "close", strerror(errno), errno);
+		return -1;
 	}
-	return main_rc;
+	return 0;
+
+end:
+	rc = close(outfile_fd);
+	if(rc == -1) {
+		fprintf(stderr, MSG_ERR_FILE, "close", strerror(errno), errno);
+	}
+	return -1;
 }
 
 /**
@@ -518,38 +508,25 @@ static int send_process(st_args params)
 	/* 送信用のソケットを準備 */
 	sock_fd = send_connect(params.hostname, params.portno);
 	if(sock_fd == -1) {
-		main_rc = -1;
-		goto end;
+		return -1;
 	}
 	
 	infile_fd = open(params.filename, O_RDONLY);
 	if(infile_fd == -1) {
-		fprintf(stderr, MSG_FILE_ERROR, "open", strerror(errno), errno);
+		fprintf(stderr, MSG_ERR_FILE, "open", strerror(errno), errno);
 		main_rc = -1;
 		goto end;
 	}
 
-	/* ソケットが書き込み可能か確認 */
 	FD_ZERO(&wfds);
 	FD_SET(sock_fd, &wfds);
 	timeout.tv_sec = TIMEOUT_SEC;
 	timeout.tv_usec = 0;
-	rc = select(sock_fd + 1, NULL, &wfds, NULL, &timeout);
-	if(rc < 0) {
-		fprintf(stderr, MSG_SELECT_ERROR, strerror(errno), errno);
-		main_rc = -1;
-		goto end;
-	}
-	if(rc == 0) {
-		fprintf(stderr, MSG_SEND_TIMEOUT);
-		main_rc = -1;
-		goto end;
-	}
 
 	while(1) {
-		read_size = read(infile_fd, buff, BUFF_SIZE - 1);
+		read_size = read(infile_fd, buff, BUFF_SIZE);
 		if(read_size == -1) {
-			fprintf(stderr, MSG_FILE_ERROR, "read", strerror(errno), errno);
+			fprintf(stderr, MSG_ERR_FILE, "read", strerror(errno), errno);
 			main_rc = -1;
 			goto end;
 		}
@@ -557,34 +534,49 @@ static int send_process(st_args params)
 			break;
 		}
 
-		buff[read_size] = '\0';
 		/* sendを最大MAX_SEND_RETRY回リトライする */
 		for(retry_count = 0;retry_count < MAX_SEND_RETRY; retry_count++) {
+			/* ソケットが書き込み可能か確認 */
+			rc = select(sock_fd + 1, NULL, &wfds, NULL, &timeout);
+			if(rc < 0) {
+				fprintf(stderr, MSG_ERR_SELECT, strerror(errno), errno);
+				main_rc = -1;
+				goto end;
+			}
+			if(rc == 0) {
+				fprintf(stderr, MSG_ERR_SEND_TIMEOUT);
+				continue;
+			}
+
 			send_size = send(sock_fd, buff, read_size, 0);
 			if(send_size > 0) {
 				break;
 			}
-			fprintf(stderr, MSG_SEND_RETRY);
+			if(errno == EINTR || EAGAIN) { /* retryしてもいいエラー */
+				fprintf(stderr, MSG_ERR_SEND_RETRY);
+				continue;
+			} else {  /* retryしても復旧しないエラー */
+				fprintf(stderr, MSG_ERR_SEND, strerror(errno), errno);
+				main_rc = -1;
+				goto end;
+			}
 		}
-		if(send_size == -1) {
-			fprintf(stderr, MSG_SEND_ERROR, strerror(errno), errno);
-			main_rc = -1;
-			goto end;
-		}
+		
 	}
 
 end:
-	if(sock_fd > 0) {
-		rc = close(sock_fd);
-		if(rc == -1) {
-			fprintf(stderr, MSG_CLOSE_ERROR, "socket", strerror(errno), errno);
-			main_rc = -1;
-		}
-	}
+
 	if(infile_fd > 0) {
 		rc= close(infile_fd);
 		if(rc == -1) {
-			fprintf(stderr, MSG_FILE_ERROR, "close", strerror(errno), errno);
+			fprintf(stderr, MSG_ERR_FILE, "close", strerror(errno), errno);
+			main_rc = -1;
+		}
+	}
+	if(sock_fd > 0) {
+		rc = close(sock_fd);
+		if(rc == -1) {
+			fprintf(stderr, MSG_ERR_CLOSE, "socket", strerror(errno), errno);
 			main_rc = -1;
 		}
 	}
@@ -602,7 +594,6 @@ end:
  */
 static int send_connect(char *hostname, unsigned short portno)
 {
-	int is_fail = 0;
 	int rc;
 	int sock_fd;
 	struct sockaddr_in server_addr;
@@ -611,15 +602,14 @@ static int send_connect(char *hostname, unsigned short portno)
 	/* ソケット作成(IP V4, TCP, プロトコルは自動選択) */
 	sock_fd = socket(AF_INET, SOCK_STREAM, 0);
 	if(sock_fd == -1) {
-		fprintf(stderr, MSG_SOCKET_ERROR, strerror(errno), errno);
+		fprintf(stderr, MSG_ERR_SOCKET, strerror(errno), errno);
 		return -1;
 	}
 	
 	/* ホスト名の解決 */
 	server = gethostbyname(hostname);
 	if(server == NULL) {
-		fprintf(stderr, MSG_NAME_RESOL_ERROR, strerror(h_errno), h_errno);
-		is_fail = 1;
+		fprintf(stderr, MSG_ERR_NAME_RESOL, strerror(h_errno), h_errno);
 		goto end;
 	}
 
@@ -631,22 +621,21 @@ static int send_connect(char *hostname, unsigned short portno)
 
 	/* 接続 */
 	rc = connect(sock_fd, (struct sockaddr*)&server_addr, sizeof(struct sockaddr));
-	if(rc == -1) {
-		fprintf(stderr, MSG_CONNECT_ERROR, strerror(errno), errno);
-		is_fail = 1;
-		goto end;
+	if(rc == 0) {
+		return sock_fd;
 	}
-end:
+	fprintf(stderr, MSG_ERR_CONNECT, strerror(errno), errno);
+	goto end;
+
+end:	
 	/* 失敗時の後始末 */
-	if(is_fail) {
-		if(sock_fd > 0) {
-			rc = close(sock_fd);
-			if(rc == -1) {
-				fprintf(stderr, MSG_CLOSE_ERROR, "socket", strerror(errno), errno);
-			}
-		}
-		return -1;
+	rc = 0;
+	if(sock_fd > 0) {
+		rc = close(sock_fd);
 	}
-	return sock_fd;
+	if(rc == -1) {
+		fprintf(stderr, MSG_ERR_CLOSE, "socket", strerror(errno), errno);
+	}
+	return -1;
 }
 
